1b6e3fa4c4ca6c08b60a8a58d10e4e6affd620ae
Delfor <64510908+delforr@users.noreply.github.com>
2023-03-07 12:30:30 -0300

    inicial commit
    Hola, ya había presentado mi trabajo, con algunos problemas entre mi css y el css que viene incluido en bootstrap así que lo hice otra vez fusionando ambos.

---
+++ main.js
@@ -0,0 +1,29 @@
+
+document.getElementById('boton_1').onclick = function(){
+
+    
+
+    document.getElementById("info_1").innerHTML = "En el año 2020 al llegar la pandemia empecé a estudiar diseño web en Youtube, estudiando HTML, CSS y Javascript";
+};
+
+document.getElementById('boton_2').onclick = function(){
+
+    
+
+    document.getElementById("info_2").innerHTML = "El primer código que estudié fue MQL4 porque trabajaba en Forex y quería automatizar las operaciones de Compra y Venta.";
+};
+
+document.getElementById('boton_3').onclick = function(){
+
+    
+
+    document.getElementById("info_3").innerHTML = "Después de estudiar MQL4 inicie con Back-End estudiando JAVA y a continuación HTML, CSS y Javascript.";
+};
+
+document.getElementById('boton_4').onclick = function(){
+
+    
+
+    document.getElementById("info_4").innerHTML = "Mi objetivo es ser programador Full - Stack. Primero maquetado de páginas web y aplicaciones móviles y luego estudiar Javascript, React y Node.js .";
+};
+
